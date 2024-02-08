dbe399e4a781b5db87e4f4b63ad8b79b1c6e4985
thomasBontempsTecrisViattech <thomas.bontemps-tecris@viattech.com>
2024-02-08 14:53:17 +0100

    Update variable

--- variable.py
+++ variable.py
@@ -1,13 +1,15 @@
+# Chemin où se situe le projet
+PATH = 'c:/Users/thomas.bontemps/Documents/Code/ResumeToDT/Code/'
 
 # Executeur de tesseract
 TESSERACT_CMD = 'C:\\Users\\thomas.bontemps\\Tesseract-OCR\\tesseract.exe'
 
 # Dossier pour stocker les différents étapes du processus
-FOLDER_RESUME = 'implementationCode/Data/resumes' # Dossier pour les CV
-FOLDER_JSON = 'implementationCode/Data/JSON/resumesJSON' # Dossier où stocker les CV sous formes JSON
-FOLDER_DOCX = 'implementationCode/Data/DOCX' # Dossier où les fichiers DOCX seront stockés
-FOLDER_IMAGE = 'implementationCode/Data/imgPdfPages' # Dossier provisoire où les CV pdf sont stockés sous forme d'image
+FOLDER_RESUME = PATH + 'implementationCode/Data/resumes' # Dossier pour les CV
+FOLDER_JSON = PATH + 'implementationCode/Data/JSON/resumesJSON' # Dossier où stocker les CV sous formes JSON
+FOLDER_DOCX = PATH + 'implementationCode/Data/DOCX' # Dossier où les fichiers DOCX seront stockés
+FOLDER_IMAGE = PATH + 'implementationCode/Data/imgPdfPages' # Dossier provisoire où les CV pdf sont stockés sous forme d'image
 
 # Element besoin pour le DT
-LOGO_VIATTECH = 'implentationCode/Data/image/viattech_qs_logo.png' # Logo viattech à utiliser lors de la création du dossier technique
+LOGO_VIATTECH = PATH + 'implentationCode/Data/image/viattech_qs_logo.png' # Logo viattech à utiliser lors de la création du dossier technique
 
