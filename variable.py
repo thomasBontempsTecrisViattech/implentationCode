955ee7373448c687aff62d37772580446ece5c72
thomasBontempsTecrisViattech <thomas.bontemps-tecris@viattech.com>
2024-02-08 17:05:32 +0100

    change variable

--- variable.py
+++ variable.py
@@ -5,13 +5,13 @@ PATH = 'c:/Users/thomas.bontemps/Documents/Code/ResumeToDT/Code/'
 TESSERACT_CMD = 'C:\\Users\\thomas.bontemps\\Tesseract-OCR\\tesseract.exe'
 
 # Dossier pour stocker les différents étapes du processus
-FOLDER_RESUME = PATH + 'implementationCode/Data/resumes' # Dossier pour les CV
-FOLDER_JSON = PATH + 'implementationCode/Data/JSON/resumesJSON' # Dossier où stocker les CV sous formes JSON
-FOLDER_DOCX = PATH + 'implementationCode/Data/DOCX' # Dossier où les fichiers DOCX seront stockés
-FOLDER_IMAGE = PATH + 'implementationCode/Data/imgPdfPages' # Dossier provisoire où les CV pdf sont stockés sous forme d'image
+FOLDER_RESUME = PATH + 'implentationCode/Data/resumes' # Dossier pour les CV
+FOLDER_JSON = PATH + 'implentationCode/Data/JSON/resumesJSON' # Dossier où stocker les CV sous formes JSON
+FOLDER_DOCX = PATH + 'implentationCode/Data/DOCX' # Dossier où les fichiers DOCX seront stockés
+FOLDER_IMAGE = PATH + 'implentationCode/Data/imgPdfPages' # Dossier provisoire où les CV pdf sont stockés sous forme d'image
 
 # Chemin où se situe le schéma du JSON 
-PATH_JSON_SCHEMA = PATH + 'implementation/Data/JSON/JSON_Schema/JsonSchema.json'
+PATH_JSON_SCHEMA = PATH + 'implentation/Data/JSON/JSON_Schema/JsonSchema.json'
 
 # Element besoin pour le DT
 LOGO_VIATTECH = PATH + 'implentationCode/Data/image/viattech_qs_logo.png' # Logo viattech à utiliser lors de la création du dossier technique
