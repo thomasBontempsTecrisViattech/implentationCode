bb0d1b6bd0b11bdb34b002570ec78c8249c9f159
thomasBontempsTecrisViattech <thomas.bontemps-tecris@viattech.com>
2024-02-13 13:26:59 +0100

    Try find best prompt

--- generateJsonFromResume.py
+++ generateJsonFromResume.py
@@ -5,23 +5,8 @@ def extract_structured_data(content: str, json_schema):
     
     template = """
     Context:
-    The resume is below and delimited by --START-- and --END--. You will use this resume to extract the information. 
-    Your role is to have an recruiter point of vue to permit of respond the best at the json schema.
-    
-    
-    Goal: 
-    Your goal is to : {goal}
-    
-    Criteria:  
-    * Recreate paragraphes with only informations wanted
-    * You will decompose the resume in distinct part to extract
-    * Use paragraph to extract most precise information or context
-    * If multiple choice as answer, create a list of answers
-    
-    Response format: 
-    * No accent at all
-    * Consistence phrase
-    
+    The resume is below and delimited by --START-- and --END--. You will use this resume to extract informations. 
+    You have to follow the section Goal and Criteria after --END--.
     
     --START--
 
@@ -29,10 +14,28 @@ def extract_structured_data(content: str, json_schema):
     
     --END--
     
+    
+    Goal: 
+    Your goal is to provide : {goal}
+    
+    Criteria:  
+    * Make sure to include all the properties mentioned
+    * Recreate paragraphes with only informations wanted
+    * You will redecompose the resume in distinct part to extract
+    * Use paragraph to extract most precise information or context
+    * Process multiple choice as answer, create a list of answers
+    
+    Response format: 
+    * No accent at all
+    * Consistence phrase
+    
+    
+
+    
     """
     
     # Fill in the placeholders in the template
-    formatted_template = template.format(goal=json_schema["description"], content=content)#, data_points=data_points)
+    formatted_template = template.format(content=content, goal=json_schema["description"])
     
     # Generate text using the formatted template
     results = llm.generate(prompt=formatted_template,
